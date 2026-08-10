aff0abc11ae736220b2b471a6880373281629cf6
Gigi-04 <kppgha001@myuct.ac.za>
2026-08-10 16:55:00 +0200

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
   plugins: [
@@ -10,5 +11,9 @@ export default defineConfig({
     react(),
     tailwindcss(),
     tsconfigPaths(),
+    sitemap({
+      hostname: "https://prettybits.co.za",
+      dynamicRoutes: ["/", "/gallery", "/contact"],
+    }),
   ],
 });
